688b48e377c16eac4b40a12bc1ec103d0849f53a
daitoch <farhanch101@outlook.com>
2022-10-07 22:20:49 +0500

    Adding sliding window solution

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -2,7 +2,12 @@ import os
 import sys
 
 def solve(data):
-    
+    maxEnding = maxSoFar = data[0]
+
+    for i in range(1, len(data)):
+        maxEnding = max(maxEnding + data[i], data[i])
+        maxSoFar = max (maxEnding, maxSoFar)
+    return maxSoFar
     pass
 
 if __name__ == "__main__":
