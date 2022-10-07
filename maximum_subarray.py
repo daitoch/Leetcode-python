75df2dc731fd5891e97773d28f30c49d9bd598c3
daitoch <farhanch101@outlook.com>
2022-10-07 14:45:57 +0500

    maximum sub array problem under progress

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-def solve(data, target):
+def solve(data):
+    
     pass
 
 if __name__ == "__main__":
     sys.stdin = open('input.txt', 'r')
     sys.stdout = open('output.txt', 'w')
-    target = int(input())
+    #target = int(input())
     data = list(map(int, input().split(' ')))
-    solve(data, target)
+    print(solve(data)) 
