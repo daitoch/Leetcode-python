50fd84b3c4f5b6f91d6bb8eb847ed6ece8b9517b
daitoch <farhanch101@outlook.com>
2022-10-07 22:25:47 +0500

    Maximum Product subarray

---
+++ max_product_subarray.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+def solve(data):
+    
+    pass
+
+if __name__ == "__main__":
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+    #target = int(input())
+    data = list(map(int, input().split(' ')))
+    print(solve(data))
