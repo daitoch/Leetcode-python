ea2e4da1f36e498730d89471c062060b69aac75c
daitoch <farhanch101@outlook.com>
2022-10-10 00:55:12 +0500

    Container with most water using kadena's two point algo

---
+++ container_with_most_water.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+def solve(data):
+    left, right = 0, len(data) - 1
+    result = 0
+    while left < right:
+        area = (right - left) * min(data[left], data[right])
+        result = max (area, result)
+        if data[left] < data[right]:
+            left += 1
+        else: 
+            right -= 1
+    return result
+    pass
+
+if __name__ == "__main__":
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+    #target = int(input())
+    data = list(map(int, input().split(' ')))
+    print(solve(data))
