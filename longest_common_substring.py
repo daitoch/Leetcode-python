363fef400f051a2f39401242ce7bc847b15dc231
daitoch <farhanch101@outlook.com>
2022-10-15 16:14:35 +0500

    Longest substring with DP and loops

--- longest_common_substring.py
+++ longest_common_substring.py
@@ -1,4 +1,17 @@
+from random import randrange
 import sys, os
+
+def solve_dp(data, target):
+    # Solving the problem by saving all the answers in a
+    # grid and hence utilizing DP paradgim
+    DP_grid  = [[0 for i in range(len(target) + 1)] for j in range(len(data) + 1)]
+    for i in range(len(data) - 1, -1, -1):
+        for j in range(len(target) -1, -1, -1):
+            if data[i] == target[j]:
+                DP_grid[i][j] = 1 + DP_grid[i+1][j+1]
+            else:
+                DP_grid[i][j] = max(DP_grid[i][j+1], DP_grid[i+1][j])
+    return DP_grid[0][0]
 
 def solve(data, target):
     if len(data) == 0 or len(target) == 0:
@@ -15,4 +28,5 @@ if __name__ == "__main__":
     sys.stdout = open('output.txt', 'w')
     target = str(input())
     data = str(input())
+    print(solve_dp(data, target))
     print(solve(data, target))
