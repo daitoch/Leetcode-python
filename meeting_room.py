adad8b5dcccf41fc7003d60ae3d93ba2d9925ae7
daitoch <farhanch101@outlook.com>
2022-11-04 18:13:20 +0500

    Meeting room solved using the same method as interval problems

---
+++ meeting_room.py
@@ -0,0 +1,23 @@
+import os, sys
+
+def solve(data):
+    data.sort(key = lambda i : i[0])
+
+    res = True
+    last = data[0][1]
+    for start, end in data[1:]:
+        if start >= last:
+            continue
+        else:
+            return False
+            
+
+    return True
+    pass
+
+if __name__ == "__main__":
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+    #target = int(input())
+    data = [[0, 30], [5, 10], [15, 20]]
+    print(solve(data))
---
+++ meeting_room_2.py
@@ -0,0 +1,11 @@
+import os, sys
+
+def solve(data, target):
+    pass
+
+if __name__ == "__main__":
+    sys.stdin = open('input.txt', 'r')
+    sys.stdout = open('output.txt', 'w')
+    target = int(input())
+    data = list(map(int, input().split(' ')))
+    print(solve(data, target))
